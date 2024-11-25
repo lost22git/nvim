5fccd35e2952d0492e8c602f8fec8a082bde0e6b
lost <lost>
2024-11-26 05:18:49 +0800

    [neovim] update

--- lua/lz/plugins/conform.lua
+++ lua/lz/plugins/conform.lua
@@ -1,12 +1,12 @@
 local M = {
-  'stevearc/conform.nvim',
+  "stevearc/conform.nvim",
   enabled = not vim.g.vscode,
-  event = { 'BufWritePre' },
+  event = { "BufWritePre" },
 }
 
 function M.config()
-  local U = require('core.utils')
-  require('conform').setup {
+  local U = require("core.utils")
+  require("conform").setup({
     format_on_save = {
       timeout_ms = 1000,
       async = false,
@@ -15,32 +15,33 @@ function M.config()
     },
     -- see :help conform-formatters
     formatters_by_ft = {
-      go         = { "goimports", "gofmt" },
-      javascript = { 'deno_fmt' },
-      python     = { 'ruff_format' },
-      crystal    = { 'crystal' },
-      sh         = { 'shfmt' },
-      nim        = { 'nph' },
-      inko       = { 'inko' },
-      roc        = { 'roc' },
-      gleam      = { 'gleam' },
-      ocaml      = { 'ocamlformat' },
-      clojure    = { 'cljfmt' },
-      fennel     = { 'fnlfmt' },
+      lua = { "stylua" },
+      go = { "goimports", "gofmt" },
+      javascript = { "deno_fmt" },
+      python = { "ruff_format" },
+      crystal = { "crystal" },
+      sh = { "shfmt" },
+      nim = { "nph" },
+      inko = { "inko" },
+      roc = { "roc" },
+      gleam = { "gleam" },
+      ocaml = { "ocamlformat" },
+      clojure = { "cljfmt" },
+      fennel = { "fnlfmt" },
     },
     formatters = {
       shfmt = {
-        command = U.get_lsp_server_path('shfmt'),
-        args = { '-l', '-w', '$FILENAME' },
+        command = U.get_lsp_server_path("shfmt"),
+        args = { "-l", "-w", "$FILENAME" },
         stdin = false,
       },
       nph = {
-        command = 'nph',
-        args = { '$FILENAME' },
+        command = "nph",
+        args = { "$FILENAME" },
         stdin = false,
       },
     },
-  }
+  })
 end
 
 return M
