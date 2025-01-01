6fe38f5c7834bf9b6256a417e75fc078b107209b
lost <lost>
2025-01-01 19:48:17 +0800

    [neovim] use mini.statusline rather than lualine

--- lua/lz/plugins/mini.lua
+++ lua/lz/plugins/mini.lua
@@ -7,7 +7,7 @@ return {
   {
     'echasnovski/mini.icons',
     opts = {},
-    lazy = true,
+    event = 'VeryLazy',
     specs = {
       { 'nvim-tree/nvim-web-devicons', enabled = false, optional = true },
     },
@@ -230,6 +230,47 @@ return {
           goto_left = '[',
           goto_right = ']',
         },
+      })
+    end,
+  },
+
+  ---------------------
+  -- mini.statusline --
+  ---------------------
+
+  {
+    'echasnovski/mini.statusline',
+    version = false,
+    event = 'VeryLazy',
+    config = function()
+      require('mini.statusline').setup({
+        content = {
+          active = function()
+            local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+            local git = MiniStatusline.section_git({ trunc_width = 40 })
+            local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+            local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+            local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+            local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+            local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+            local location = MiniStatusline.section_location({ trunc_width = 75 })
+
+            local color_scheme = MiniIcons.get('lsp', 'color') .. ' ' .. (vim.g.colors_name or 'default')
+
+            return MiniStatusline.combine_groups({
+              { hl = mode_hl, strings = { mode } },
+              { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+              '%<', -- Mark general truncate point
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
+              '%=', -- End left alignment
+              { hl = 'MiniStatuslineLocation', strings = { location } },
+              { hl = mode_hl, strings = { color_scheme, fileinfo } },
+            })
+          end,
+          inactive = nil,
+        },
+        use_icons = true,
+        set_vim_settings = true,
       })
     end,
   },
