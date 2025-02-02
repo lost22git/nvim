28c17046e80be4da1ff477abf37974d0df580e33
lost <lost>
2025-02-03 05:17:07 +0800

    [neovim] udpate

--- lua/lz/plugins/mini.lua
+++ lua/lz/plugins/mini.lua
@@ -244,37 +244,7 @@ return {
     'echasnovski/mini.statusline',
     version = false,
     event = 'VeryLazy',
-    config = function()
-      require('mini.statusline').setup({
-        content = {
-          active = function()
-            local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-            local git = MiniStatusline.section_git({ trunc_width = 40 })
-            local diff = MiniStatusline.section_diff({ trunc_width = 75 })
-            local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-            local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
-            local filename = MiniStatusline.section_filename({ trunc_width = 140 })
-            local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-            local location = MiniStatusline.section_location({ trunc_width = 75 })
-
-            local color_scheme = require('mini.icons').get('lsp', 'color') .. ' ' .. (vim.g.colors_name or 'default')
-
-            return MiniStatusline.combine_groups({
-              { hl = mode_hl, strings = { mode } },
-              { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
-              '%<', -- Mark general truncate point
-              { hl = 'MiniStatuslineFilename', strings = { filename } },
-              '%=', -- End left alignment
-              { hl = 'MiniStatuslineLocation', strings = { location } },
-              { hl = mode_hl, strings = { color_scheme, fileinfo } },
-            })
-          end,
-          inactive = nil,
-        },
-        use_icons = true,
-        set_vim_settings = true,
-      })
-    end,
+    config = function() require('mini.statusline').setup({}) end,
   },
 
   -----------------
@@ -291,6 +261,7 @@ return {
   ------------
   -- notify --
   ------------
+
   {
     'echasnovski/mini.notify',
     version = false,
