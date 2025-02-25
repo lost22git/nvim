02012bc08f719ecbee45d589d905d533ad298a49
lost <lost>
2025-02-26 07:47:58 +0800

    [neovim] update

--- lua/lz/plugins.lua
+++ lua/lz/plugins.lua
@@ -282,5 +282,27 @@ return {
   ---------------------
   -- better quickfix --
   ---------------------
-  {},
+
+  {
+    'stevearc/quicker.nvim',
+    ft = 'qf',
+    kesy = {
+      { '<Leader>q', function() require('quicker').toggle() end, desc = 'Toggle quickfix' },
+      { '<Leader>l', function() require('quicker').toggle({ loclist = true }) end, desc = 'Toggle loclist' },
+    },
+    opts = {
+      keys = {
+        {
+          '>',
+          function() require('quicker').expand({ before = 2, after = 2, add_to_existing = true }) end,
+          desc = 'Expand quickfix context',
+        },
+        {
+          '<',
+          function() require('quicker').collapse() end,
+          desc = 'Collapse quickfix context',
+        },
+      },
+    },
+  },
 }
