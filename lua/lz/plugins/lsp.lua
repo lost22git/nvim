b6329a6e8e1814c57b4da1b915c40820b19732a8
lost <lost>
2025-04-29 21:26:58 +0800

    [neovim] fix lua_ls config

--- lua/lz/plugins/lsp.lua
+++ lua/lz/plugins/lsp.lua
@@ -1,6 +1,6 @@
-local function create_user_command_LualsRestart()
+local function create_LualsReloadNvim_command()
   local callback = function(input)
-    local library_map = {
+    local nvim_library_map = {
       vim = {
         vim.env.VIMRUNTIME,
         '${3rd}/luv/library',
@@ -13,23 +13,46 @@ local function create_user_command_LualsRestart()
 
     local mode, force = unpack(vim.fn.split(input.fargs[1], '-', false))
 
-    local new_val, old_val = library_map[mode], vim.g.lua_ls_settings_workspace_library
+    local new_val, old_val = nvim_library_map[mode], vim.g.lua_ls_reload_nvim
 
     if force ~= 'force' and require('core.utils').tbl_includes(old_val, new_val) then
-      vim.notify('[LualsRestart] modules included, nothing todo')
+      vim.notify('[LualsReloadNvim] modules have already loaded, nothing todo')
     else
-      vim.g.lua_ls_settings_workspace_library = new_val
+      vim.g.lua_ls_reload_nvim = new_val
       vim.cmd([[ LspRestart lua_ls ]])
     end
   end
 
-  local opts = {
+  vim.api.nvim_create_user_command('LualsReloadNvim', callback, {
     nargs = 1,
-    complete = function(_, _, _) return { 'vim', 'all', 'vim-force', 'all-force' } end,
-  }
-
-  vim.api.nvim_create_user_command('LualsRestart', callback, opts)
+    complete = function() return { 'vim', 'all', 'vim-force', 'all-force' } end,
+  })
 end
+
+local lua_conditional_settings = {
+  {
+    cond = function(nvim_config_path, workspace_path)
+      return not workspace_path
+        or nvim_config_path == workspace_path:sub(1, #nvim_config_path)
+        or not (vim.uv.fs_stat(workspace_path .. '/.luarc.json') or vim.uv.fs_stat(workspace_path .. '/.luarc.jsonc'))
+    end,
+    config = function(client)
+      print('lua_ls load nvim modules')
+      vim.g.lua_ls_reload_nvim = vim.g.lua_ls_reload_nvim
+        or {
+          vim.env.VIMRUNTIME,
+          '${3rd}/luv/library',
+        }
+      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        codeLens = { enable = false },
+        runtime = { version = 'LuaJIT' },
+        workspace = { checkThirdParty = false, library = vim.g.lua_ls_reload_nvim },
+      })
+
+      create_LualsReloadNvim_command()
+    end,
+  },
+}
 
 local M = {
   'neovim/nvim-lspconfig',
@@ -79,55 +102,32 @@ function M.config()
   lspconfig.lua_ls.setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    settings = {
-      Lua = {
+    on_init = function(client)
+      local nvim_config_path = vim.fn.stdpath('config')
+      local nvim_config_real_path =
+        vim.uv.fs_realpath(type(nvim_config_path) == 'table' and nvim_config_path[1] or tostring(nvim_config_path))
+      print('lua_ls nvim config path:', nvim_config_real_path)
+
+      local workspace_path = nil
+      if client.workspace_folders then workspace_path = vim.uv.fs_realpath(client.workspace_folders[1].name) end
+      print('lua_ls workspace path:', workspace_path)
+
+      -- default settings
+      client.config.settings.Lua = {
         codeLens = { enable = true },
         completion = { callSnippet = 'Replace' },
         telemetry = { enable = false },
         workspace = { checkThirdParty = false, library = {} },
-      },
-    },
-    on_init = function(client)
-      if client.workspace_folders then
-        local path = vim.uv.fs_realpath(client.workspace_folders[1].name)
-        local nvim_config_path = vim.fn.stdpath('config')
+      }
 
-        print('lua_ls workspace path:', path)
-
-        local nvim_config_real_path =
-          vim.uv.fs_realpath(type(nvim_config_path) == 'table' and nvim_config_path[1] or tostring(nvim_config_path))
-
-        print('lua_ls nvim config path:', nvim_config_real_path)
-
-        local normal_lua_project = path and nvim_config_real_path ~= path:sub(1, #nvim_config_real_path)
-        if normal_lua_project then
-          -- Use `client.config.settings.Lua`
-          print('It is a normal lua project')
-          print('lua_ls settings:', vim.inspect(client.config.settings.Lua))
-          return
+      -- conditional settings
+      for _, s in ipairs(lua_conditional_settings) do
+        if s.cond(nvim_config_real_path, workspace_path) then
+          s.config(client)
+          break
         end
       end
 
-      -- If dev/config Neovim or single file
-      -- Add these settings
-      print('lua_ls load vim modules')
-      vim.g.lua_ls_settings_workspace_library = vim.g.lua_ls_settings_workspace_library
-        or {
-          vim.env.VIMRUNTIME,
-          '${3rd}/luv/library',
-        }
-      client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-        codeLens = { enable = false },
-        runtime = {
-          version = 'LuaJIT',
-        },
-        workspace = {
-          checkThirdParty = false,
-          library = vim.g.lua_ls_settings_workspace_library,
-        },
-      })
-
-      create_user_command_LualsRestart()
       print('lua_ls settings:', vim.inspect(client.config.settings.Lua))
     end,
   })
