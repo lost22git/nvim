1e1279385c22fac454af878411eb76c3ee18d58c
lost <lost>
2025-08-03 00:52:07 +0800

    [neovim] remove lua_ls configuration

--- fnl/lz/plugins/lsp.fnl
+++ fnl/lz/plugins/lsp.fnl
@@ -1,56 +1,7 @@
 (import-macros {: autocmd! : usercmd!} :config.macros)
 
-(local {: list_includes : get_mason_path : lsp_on_attach : lsp_capabilities}
+(local {: get_mason_path : lsp_on_attach : lsp_capabilities}
        (require :core.utils))
-
-(fn create_usercmd_LuaLibsReload []
-  (fn callback [input]
-    (local libs
-           {:vim [vim.env.VIMRUNTIME "${3rd}/luv/library"]
-            :all ["${3rd}/luv/library"
-                  (unpack (vim.api.nvim_get_runtime_file "" true))]})
-    (local [mode force] (vim.fn.split (. input.fargs 1) "-" false))
-    (local (new_libs old_libs) (values (. libs mode) vim.g.nvim_lua_libs))
-    (if (and (not= force :force) (list_includes old_libs new_libs))
-        (vim.notify "[LuaLibsReload] libs have already loaded.")
-        (do
-          (set vim.g.nvim_lua_libs new_libs)
-          (vim.cmd "LspRestart lua_ls"))))
-
-  (usercmd! :LuaLibsReload callback
-            {:nargs 1 :complete #[:vim :all :vim-force :all-force]}))
-
-(local lua_conditional_settings
-       [{:match (fn [nvim_config_path workspace_path]
-                  (or (not workspace_path)
-                      (vim.startswith workspace_path nvim_config_path)
-                      (not (or (vim.uv.fs_stat (.. workspace_path
-                                                   "/.luarc.json"))
-                               (vim.uv.fs_stat (.. workspace_path
-                                                   "/.luarc.jsonc"))))))
-         :config (fn [client]
-                   (print "lua_ls load nvim lua libs.")
-                   (set vim.g.nvim_lua_libs
-                        (or vim.g.nvim_lua_libs
-                            [vim.env.VIMRUNTIME "${3rd}/luv/library"]))
-                   (set client.config.settings.Lua
-                        (vim.tbl_deep_extend :force client.config.settings.Lua
-                                             {:codeLens {:enable false}
-                                              :runtime {:version :LuaJIT}
-                                              :workspace {:checkThirdParty false
-                                                          :library vim.g.nvim_lua_libs}}))
-                   (create_usercmd_LuaLibsReload))}])
-
-(fn get_nvim_config_path []
-  (let [path (vim.fn.stdpath :config)
-        path1 (case (type path)
-                :table (. path 1)
-                _ (tostring path))]
-    (vim.uv.fs_realpath path1)))
-
-(fn get_workspace_path [client]
-  (when client.workspace_folders
-    (vim.uv.fs_realpath (. client.workspace_folders 1 :name))))
 
 [{1 "neovim/nvim-lspconfig"
   :cmd [:LspInfo :LspStart :LspLog]
@@ -72,32 +23,6 @@
                                       (vim.lsp.get_client_by_id)
                                       (assert)
                                       (lsp_on_attach $.buf))})
-            ;; lua_ls
-            (vim.lsp.config :lua_ls
-                            {:settings {:Lua {:codeLens {:enable true}
-                                              :completion {:callSnippet :Replace}
-                                              :telemetry {:enable false}
-                                              :workspace {:checkThirdParty false
-                                                          :library {}}}}
-                             :on_init (fn [client]
-                                        ;; nvim_config_path
-                                        (local nvim_config_path
-                                               (get_nvim_config_path))
-                                        (print "lua_ls nvim config path:"
-                                               nvim_config_path)
-                                        ;; workspace_path
-                                        (local workspace_path
-                                               (get_workspace_path client))
-                                        (print "lua_ls workspace path:"
-                                               workspace_path)
-                                        ;; conditional settings
-                                        (each [_ s (ipairs lua_conditional_settings)]
-                                          (when (s.match nvim_config_path
-                                                         workspace_path)
-                                            (s.config client)
-                                            (lua "break")))
-                                        (print "lua_ls settings:"
-                                               (vim.inspect client.config.settings.Lua)))})
             ;; flix
             (vim.lsp.config :flix
                             {:cmd ["flix" "lsp"]
--- lua/lz/plugins/lsp.lua
+++ lua/lz/plugins/lsp.lua
@@ -1,82 +1,15 @@
 -- [nfnl] fnl/lz/plugins/lsp.fnl
 local _local_1_ = require("core.utils")
-local list_includes = _local_1_["list_includes"]
 local get_mason_path = _local_1_["get_mason_path"]
 local lsp_on_attach = _local_1_["lsp_on_attach"]
 local lsp_capabilities = _local_1_["lsp_capabilities"]
-local function create_usercmd_LuaLibsReload()
-  local function callback(input)
-    local libs = {vim = {vim.env.VIMRUNTIME, "${3rd}/luv/library"}, all = {"${3rd}/luv/library", unpack(vim.api.nvim_get_runtime_file("", true))}}
-    local _local_2_ = vim.fn.split(input.fargs[1], "-", false)
-    local mode = _local_2_[1]
-    local force = _local_2_[2]
-    local new_libs, old_libs = libs[mode], vim.g.nvim_lua_libs
-    if ((force ~= "force") and list_includes(old_libs, new_libs)) then
-      return vim.notify("[LuaLibsReload] libs have already loaded.")
-    else
-      vim.g.nvim_lua_libs = new_libs
-      return vim.cmd("LspRestart lua_ls")
-    end
-  end
-  local function _4_()
-    return {"vim", "all", "vim-force", "all-force"}
-  end
-  return vim.api.nvim_create_user_command("LuaLibsReload", callback, {nargs = 1, complete = _4_})
-end
-local lua_conditional_settings
-local function _5_(nvim_config_path, workspace_path)
-  return (not workspace_path or vim.startswith(workspace_path, nvim_config_path) or not (vim.uv.fs_stat((workspace_path .. "/.luarc.json")) or vim.uv.fs_stat((workspace_path .. "/.luarc.jsonc"))))
-end
-local function _6_(client)
-  print("lua_ls load nvim lua libs.")
-  vim.g.nvim_lua_libs = (vim.g.nvim_lua_libs or {vim.env.VIMRUNTIME, "${3rd}/luv/library"})
-  client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {codeLens = {enable = false}, runtime = {version = "LuaJIT"}, workspace = {library = vim.g.nvim_lua_libs, checkThirdParty = false}})
-  return create_usercmd_LuaLibsReload()
-end
-lua_conditional_settings = {{match = _5_, config = _6_}}
-local function get_nvim_config_path()
-  local path = vim.fn.stdpath("config")
-  local path1
-  do
-    local _7_ = type(path)
-    if (_7_ == "table") then
-      path1 = path[1]
-    else
-      local _ = _7_
-      path1 = tostring(path)
-    end
-  end
-  return vim.uv.fs_realpath(path1)
-end
-local function get_workspace_path(client)
-  if client.workspace_folders then
-    return vim.uv.fs_realpath(client.workspace_folders[1].name)
-  else
-    return nil
-  end
-end
-local function _10_()
+local function _2_()
   vim.diagnostic.config({severity_sort = true, float = true, jump = {float = true}, virtual_text = false})
   vim.lsp.config("*", {root_markers = {".git"}, capabilities = lsp_capabilities()})
-  local function _11_(_241)
+  local function _3_(_241)
     return lsp_on_attach(assert(vim.lsp.get_client_by_id(_241.data.client_id)), _241.buf)
   end
-  vim.api.nvim_create_autocmd("LspAttach", {callback = _11_})
-  local function _12_(client)
-    local nvim_config_path = get_nvim_config_path()
-    print("lua_ls nvim config path:", nvim_config_path)
-    local workspace_path = get_workspace_path(client)
-    print("lua_ls workspace path:", workspace_path)
-    for _, s in ipairs(lua_conditional_settings) do
-      if s.match(nvim_config_path, workspace_path) then
-        s.config(client)
-        break
-      else
-      end
-    end
-    return print("lua_ls settings:", vim.inspect(client.config.settings.Lua))
-  end
-  vim.lsp.config("lua_ls", {settings = {Lua = {codeLens = {enable = true}, completion = {callSnippet = "Replace"}, telemetry = {enable = false}, workspace = {library = {}, checkThirdParty = false}}}, on_init = _12_})
+  vim.api.nvim_create_autocmd("LspAttach", {callback = _3_})
   vim.lsp.config("flix", {cmd = {"flix", "lsp"}, filetypes = {"flix"}, root_markers = {"flix.toml"}})
   vim.lsp.config("nim_langserver", {settings = {nim = {inlayHints = {exceptionHints = {enable = false}}}}})
   vim.lsp.config("raku_navigator", {cmd = {"raku-navigator", "--stdio"}})
@@ -84,4 +17,4 @@ local function _10_()
   vim.lsp.config("zls", {settings = {zls = {enable_snippets = true, highlight_global_var_declarations = true, enable_argument_placeholders = false}}})
   return vim.lsp.enable({"emmylua_ls", "marksman", "kulala_ls", "dockerls", "nushell", "powershell_es", "html", "htmx", "svelte", "vtsls", "clojure_lsp", "crystalline", "dartls", "elixirls", "fennel_ls", "gleam", "gradle_ls", "gopls", "julials", "koka", "kotlin_lsp", "ocamllsp", "ols", "racket_langserver", "roc_ls", "ruff", "sourcekit", "v_analyzer"})
 end
-return {{"neovim/nvim-lspconfig", cmd = {"LspInfo", "LspStart", "LspLog"}, dependencies = {{"deathbeam/lspecho.nvim", opts = {}}, {"rachartier/tiny-inline-diagnostic.nvim", opts = {preset = "ghost"}}}, config = _10_}, {"williamboman/mason.nvim", cmd = "Mason", opts = {install_root_dir = get_mason_path(), PATH = "prepend", ui = {backdrop = vim.g.zz.backdrop}}}}
+return {{"neovim/nvim-lspconfig", cmd = {"LspInfo", "LspStart", "LspLog"}, dependencies = {{"deathbeam/lspecho.nvim", opts = {}}, {"rachartier/tiny-inline-diagnostic.nvim", opts = {preset = "ghost"}}}, config = _2_}, {"williamboman/mason.nvim", cmd = "Mason", opts = {install_root_dir = get_mason_path(), PATH = "prepend", ui = {backdrop = vim.g.zz.backdrop}}}}
