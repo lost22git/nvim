4f2baba386a5ef41bf50f02ee45a49d814aa10a1
lost <lost>
2026-04-23 17:18:37 +0800

    remove FTerm plugin

--- lua/lz/plugins/term.lua
+++
@@ -1,2 +0,0 @@
--- [nfnl] fnl/lz/plugins/term.fnl
-return {{"numToStr/FTerm.nvim", keys = {{"<M-3>", "<C-\\><C-n><Cmd>lua require(\"FTerm\").toggle()<CR>", mode = {"n", "v", "i", "t"}, noremap = true, desc = "[FTerm] Toggle"}}, opts = {ft = "FTerm", cmd = (vim.g.zz.shell or vim.o.shell), border = vim.o.winborder}}}
